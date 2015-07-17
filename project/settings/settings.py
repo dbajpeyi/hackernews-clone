76269f4f4635284d8a55fdd758dbe92877fdfd5f
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2015-07-18 05:06:03 +0530

    call it vendor

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-   os.path.join(PROJECT_ROOT, '..', 'static'), 
+   os.path.join(PROJECT_ROOT, '..', 'vendor'), 
 )
 
 
