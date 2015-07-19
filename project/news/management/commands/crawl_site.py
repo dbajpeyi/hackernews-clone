a5c7774bc5974fd3087f1ddd55210e2776f6db3d
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2015-07-20 01:15:36 +0530

    init crawl

--- project/news/management/commands/crawl_site.py
+++ project/news/management/commands/crawl_site.py
@@ -1,8 +1,29 @@
 from django.core.management.base import BaseCommand, CommandError
 from news.models import Item
+import requests
+from bs4 import BeautifulSoup
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-	print "Scraping websites"
+    page = {}
 
+    def handle(self, *args, **options):
+        for i in range(1,4):
+            self.page_no = i
+            self.parse()
+
+
+    def get_result(self): 
+        return requests.get('https://news.ycombinator.com/news?p=%s'% self.page_no)
+
+    def parse(self):
+        soup = BeautifulSoup(self.get_result().text, 'html.parser')
+        self.page[self.page_no] = []
+        for x in soup.find_all('table')[2].find_all('tr'):
+            title = x.find_all('td', 'title')
+            if title:
+                try:
+                    self.page[self.page_no].append(title[1].a)
+                    print title[1].a
+                except IndexError:
+                    print 'Done page %s'%self.page_no
