91e78c040e3297afccfe169128895d9d4be7fe06
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2015-07-20 02:37:47 +0530

    crawl votes, user and time

--- project/news/management/commands/crawl_site.py
+++ project/news/management/commands/crawl_site.py
@@ -6,11 +6,14 @@ from bs4 import BeautifulSoup
 class Command(BaseCommand):
 
     page = {}
+    td_count = 2
+    data_count = 0
 
     def handle(self, *args, **options):
         for i in range(1,4):
             self.page_no = i
             self.parse()
+        print self.page[1]
 
 
     def get_result(self): 
@@ -18,12 +21,21 @@ class Command(BaseCommand):
 
     def parse(self):
         soup = BeautifulSoup(self.get_result().text, 'html.parser')
-        self.page[self.page_no] = []
         for x in soup.find_all('table')[2].find_all('tr'):
+            self.data_count += 1 
+            self.page[self.data_count] = {'other_data' : None, 'url' : ''}
+            if self.td_count%3 == 0:
+                try:
+                    subtext = x.find_all('td','subtext')[0]
+                    self.page[self.data_count - 1]['other_data'] = subtext
+                except IndexError:
+                    pass
+
             title = x.find_all('td', 'title')
             if title:
                 try:
-                    self.page[self.page_no].append(title[1].a)
+                    self.page[self.data_count]['url'] = title[1].a
                     print title[1].a
                 except IndexError:
                     print 'Done page %s'%self.page_no
+            self.td_count +=1
