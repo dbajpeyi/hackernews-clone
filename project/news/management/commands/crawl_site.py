e25db5c928758b80da7734a3ca196aa2fa807a73
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2015-07-19 17:58:50 +0530

    management command scaffold

---
+++ project/news/management/commands/crawl_site.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+from news.models import Item
+
+class Command(BaseCommand):
+
+	def handle(self, *args, **options):
+		print "Scraping websites"
+
